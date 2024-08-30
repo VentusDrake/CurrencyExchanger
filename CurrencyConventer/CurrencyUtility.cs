efca7f70643f26b4e7a9f84b349e98a5b1676187
Ventus <theraider789@gmail.com>
2024-08-30 22:07:41 +0200

    Added connection to API and made changes to converting currency

---
+++ CurrencyConventer/CurrencyUtility.cs
@@ -0,0 +1,17 @@
+﻿using CurrencyConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConventer
+{
+    static class CurrencyUtility
+    {
+        public static double Convert(double amountToConvert, double currencyRate)
+        {
+            return amountToConvert * currencyRate;
+        }
+    }
+}
--- CurrencyConventer/Program.cs
+++ CurrencyConventer/Program.cs
@@ -1,1 +1,81 @@
-﻿Console.WriteLine("Hello World!");
+﻿using System;
+using CurrencyConventer;
+using Newtonsoft.Json.Linq;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        static async Task Main(String[] args)
+        {
+            /// <summary>
+            /// Free API allows only changing rates from USD to every other
+            /// </summary>
+            String apiKey = "2fb045b979494ca0843e123c2c006210";
+            String apiUrl = $"https://openexchangerates.org/api/latest.json?app_id={apiKey}";
+            
+            String data = await GetDataAsync(apiUrl);
+            JObject json = JObject.Parse(data);
+
+            Console.WriteLine("=====================================");
+            Console.WriteLine("Welcome to currency converter!");
+            Console.WriteLine("Your money is in USD!");
+            Console.WriteLine("=====================================");
+
+            Console.WriteLine("To which currency you want convert?");
+            showCurrencies();
+            Currency responseCurrency = (Currency)(Convert.ToInt32(Console.ReadLine()) - 1);
+
+            Console.WriteLine("How much do you want to convert: ");
+            double amountToConvert = Convert.ToDouble(Console.ReadLine());
+
+            double currencyRate = json["rates"][responseCurrency.ToString()].Value<double>();
+            double convertedAmount = CurrencyUtility.Convert(amountToConvert, currencyRate);
+
+            Console.WriteLine($"You have {convertedAmount}{responseCurrency}");
+        }
+
+        static async Task<String> GetDataAsync(String apiUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    String responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+
+                }
+                catch (HttpRequestException e)
+                {
+                    return $"Wystąpił błąd podczas wysyłania żądania {e.Message}";
+                }
+            }
+        }
+
+        static void showCurrencies()
+        {
+            var currencies = Enum.GetValues(typeof(Currency));
+            int index = 1;
+            foreach (var currency in currencies)
+            {
+                Console.WriteLine($"{index}) {currency}");
+                index++;
+            }
+        }
+    }
+
+    enum Currency
+    {
+        EUR,
+        GBP,
+        PLN,
+        JPY,
+        AUD,
+        CAD,
+        CHF,
+        CNY
+    }
+}
